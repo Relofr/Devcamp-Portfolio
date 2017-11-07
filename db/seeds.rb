e3d4b96487d6a68e2a618446e64a283bdbce90a1
Kyle Hatch <ky13h4tch@gmail.com>
2017-11-07 13:59:00 -0700

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,21 @@
 User.create!(
   email: 'admin@admin.com',
-  name: 'Kyle Hatch',
+  name: 'Site Admin',
   password: 'asdfasdf', 
   password_confirmation: 'asdfasdf',
   roles: 'site_admin'
   )
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "regular@regular.com",
+  name: "Regular User",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+)
+
+puts "1 regular user created"
 
 3.times do |topic|
   Topic.create!(
